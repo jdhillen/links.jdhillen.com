a4c0467dc504196ce86aff18e669181f3c7800ec
J.D. Hillen <jdhillen@dimin.com>
2025-04-15 14:40:40 -0500

    fix(release-config): update release-config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,12 @@
 export default {
-  extends: '@jdhillen/release-config',
   branches: ['main'],
-  tagFormat: '${version}'
+  tagFormat: '${version}',
+  extends: '@jdhillen/release-config',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/github'
+  ]
 };
